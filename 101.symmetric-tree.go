23cfb45b4a5c01b6af9b38ee60abec936a1dac12
lorrainema <lorrainema@tencent.com>
2020-05-13 17:52:33 +0800

    add solutions

---
+++ 101.symmetric-tree.go
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=101 lang=golang
+ *
+ * [101] Symmetric Tree
+ *
+ * https://leetcode.com/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (45.98%)
+ * Likes:    3665
+ * Dislikes: 87
+ * Total Accepted:    606.2K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric
+ * around its center).
+ *
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠/ \ / \
+ * 3  4 4  3
+ *
+ *
+ *
+ *
+ * But the following [1,2,2,null,3,null,3] is not:
+ *
+ *
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   2
+ * ⁠  \   \
+ * ⁠  3    3
+ *
+ *
+ *
+ *
+ * Follow up: Solve it both recursively and iteratively.
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return check(root.Left, root.Right)
+}
+
+func check(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if (left == nil && right != nil) || (left != nil && right == nil) {
+		return false
+	}
+
+	return (left.Val == right.Val) && check(left.Left, right.Right) && check(left.Right, right.Left)
+}
+
+// @lc code=end
+
